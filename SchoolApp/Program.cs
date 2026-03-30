6f1ae060ded4f04b8d5166edb279c9665364683c
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-30 21:17:23 +0300

    chore(program): add repositories to IOC Container

--- SchoolApp/Program.cs
+++ SchoolApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SchoolApp.Data;
+using SchoolApp.Repositories;
 
 namespace SchoolApp
 {
@@ -14,6 +15,8 @@ namespace SchoolApp
             // Scoped - per request
             builder.Services.AddDbContext<SchoolMvc9Context>(options =>
                 options.UseSqlServer(connString));
+
+            builder.Services.AddRepositories();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
