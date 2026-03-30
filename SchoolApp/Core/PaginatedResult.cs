d7f3532dd74e9491e1858c31ff1f5c79b8a29723
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-30 19:53:21 +0300

    chore(core): alter paginated result, and add a default contructor

---
+++ SchoolApp/Core/PaginatedResult.cs
@@ -0,0 +1,23 @@
+﻿namespace SchoolApp.Core
+{
+    public class PaginatedResult<T>
+    {
+        public List<T> Data { get; set; } = [];
+
+        public int TotalRecords { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalPages => (int)Math.Ceiling((double)TotalRecords / PageSize);
+
+        public PaginatedResult(List<T> data, int totalRecords, int pageNumber, int pageSize)
+        {
+                Data = data;
+                TotalRecords = totalRecords;
+                PageNumber = pageNumber;
+                PageSize = pageSize;
+        }
+    }
+}
