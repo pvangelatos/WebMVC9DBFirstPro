0db82b9fdd3efe75c6203d764343670a96949656
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-30 21:16:53 +0300

    chore(repositories): add unit of work design pattern

---
+++ SchoolApp/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolApp.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+        ITeacherRepository TeacherRepository { get; }
+        IStudentRepository StudentRepository { get; }
+        ICourseRepository CourseRepository { get; }
+
+        Task<bool> SaveAsync();
+    }
+}
---
+++ SchoolApp/Repositories/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using SchoolApp.Data;
+
+namespace SchoolApp.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SchoolMvc9Context _context;
+        public IUserRepository UserRepository { get; }
+        public ITeacherRepository TeacherRepository { get; }
+        public IStudentRepository StudentRepository { get; }
+        public ICourseRepository CourseRepository { get; }
+
+        public UnitOfWork(SchoolMvc9Context context)
+        {
+            _context = context;
+            UserRepository = new UserRepository(context);
+            TeacherRepository = new TeacherRepository(context);
+            StudentRepository = new StudentRepository(context);
+            CourseRepository = new CourseRepository(context);
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;   // commit & rollback
+        }
+    }
+}
