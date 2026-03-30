d2f3d111de1fba9460869abcf81291dad8fc0020
pvangelatos <p.vangelatos23@hotmail.com>
2026-03-30 20:10:35 +0300

    chore(repositories): add user repository

---
+++ SchoolApp/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using SchoolApp.Core;
+using SchoolApp.Models;
+using System.Linq.Expressions;
+
+namespace SchoolApp.Repositories
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<User?> GetByUsernameAsync(string username);
+        Task<PaginatedResult<User>> GetUsersAsync(int pageNumber, int pageSize,
+            List<Expression<Func<User, bool>>> predicates);
+    }
+}
---
+++ SchoolApp/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolApp.Core;
+using SchoolApp.Data;
+using SchoolApp.Models;
+using System.Linq.Expressions;
+
+namespace SchoolApp.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(SchoolMvc9Context context) : base(context)
+        {
+        }
+        public async Task<User?> GetByUsernameAsync(string username) => 
+            await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+
+        public async Task<PaginatedResult<User>> GetUsersAsync(int pageNumber, int pageSize, 
+            List<Expression<Func<User, bool>>> predicates)
+        {
+            int totalRecords;
+            IQueryable<User> query = _context.Users;
+
+            if (predicates != null && predicates.Count > 0)
+            {
+                foreach (var predicate in predicates)
+                {
+                    query = query.Where(predicate);  // υπονοείται το AND
+                }
+            }
+
+            totalRecords = await query.CountAsync();
+            int skip = (pageNumber - 1) * pageSize;
+
+            var data = await query
+                .OrderBy(u => u.Id)     // Πάντα Order by για σταθερή σειρά
+                .Skip(skip)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PaginatedResult<User>
+            {
+                Data = data,
+                TotalRecords = totalRecords,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+        }
+    }
+}
