345d4ab3d4556a4f536eb618cef500c6fbe71cec
pvangelatos <p.vangelatos23@hotmail.com>
2026-04-18 14:57:30 +0300

    chore(core): add filters dto

---
+++ SchoolApp/Core/Filters/UserFiltersDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolApp.Core.Filters
+{
+    public class UserFiltersDTO
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? UserRole { get; set; }
+    }
+}
