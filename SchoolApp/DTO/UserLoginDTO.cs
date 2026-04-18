437db8ce67469160a8cddd0c610173d07eefcb2a
pvangelatos <p.vangelatos23@hotmail.com>
2026-04-18 13:37:42 +0300

    chore(DTO): add dto for teacher signup, login and user readonly dto

---
+++ SchoolApp/DTO/UserLoginDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolApp.DTO
+{
+    public record UserLoginDTO
+    {
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [RegularExpression(@"(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?\W)^.{8,}$",
+            ErrorMessage = "Password must contain at least one uppercase, one lowercase, " +
+            "one digit, and one special character")]
+        public string? Password { get; set; }
+
+        public bool KeepLoggedIn { get; set; }
+    }
+}
---
+++ SchoolApp/DTO/UserReadOnlyDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolApp.DTO
+{
+    public record UserReadOnlyDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+        public string UserRole { get; set; } = null!;
+    }
+}
