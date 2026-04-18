03315e385715c924611858195a5670b00726eac0
pvangelatos <p.vangelatos23@hotmail.com>
2026-04-18 14:02:37 +0300

    add automapper config

---
+++ SchoolApp/Configuration/MapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SchoolApp.DTO;
+using SchoolApp.Models;
+
+namespace SchoolApp.Configuration
+{
+    public class MapperConfig : Profile
+
+    {
+        public MapperConfig()
+        {
+            CreateMap<User, UserReadOnlyDTO>()
+                .ForMember(dest => dest.UserRole, opt => opt.MapFrom(src => src.Role.Name));
+
+            CreateMap<TeacherSignupDTO, User>()
+                .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.RoleId!.Value));
+        }
+
+    }
+}
